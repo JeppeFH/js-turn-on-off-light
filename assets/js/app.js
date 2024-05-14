bc768cb95fecc969525dde1baa55030bf812bdc2
JeppeFH <jeppefinch@hotmail.dk>
2024-05-14 10:35:36 +0200

    Vertical Mobile Navbar

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,7 +12,51 @@ function on() {
 }
 
 /*Faq*/
-function show() {
-  document.getElementById("faq-btn-on").style.display = "block";
-  document.getElementById("faq-btn-off").style.display = "none";
+function faqBtnOff_1() {
+  document.getElementById("faqBtnOn_1").style.display = "block";
+  document.getElementById("faqBtnOff_1").style.display = "none";
+  document.getElementById("faqHide_1").style.display = "none";
+}
+
+function faqBtnOn_1() {
+  document.getElementById("faqBtnOn_1").style.display = "none";
+  document.getElementById("faqBtnOff_1").style.display = "block";
+  document.getElementById("faqHide_1").style.display = "block";
+}
+
+function faqBtnOff_2() {
+  document.getElementById("faqBtnOn_2").style.display = "block";
+  document.getElementById("faqBtnOff_2").style.display = "none";
+  document.getElementById("faqHide_2").style.display = "none";
+}
+
+function faqBtnOn_2() {
+  document.getElementById("faqBtnOn_2").style.display = "none";
+  document.getElementById("faqBtnOff_2").style.display = "block";
+  document.getElementById("faqHide_2").style.display = "block";
+}
+
+function faqBtnOff_3() {
+  document.getElementById("faqBtnOn_3").style.display = "block";
+  document.getElementById("faqBtnOff_3").style.display = "none";
+  document.getElementById("faqHide_3").style.display = "none";
+}
+
+function faqBtnOn_3() {
+  document.getElementById("faqBtnOn_3").style.display = "none";
+  document.getElementById("faqBtnOff_3").style.display = "block";
+  document.getElementById("faqHide_3").style.display = "block";
+}
+
+/*Mobile navbar*/
+function burgermenu_off() {
+  document.getElementById("nav-links").style.display = "block";
+  document.getElementById("burgerNav_off").style.display = "none";
+  document.getElementById("burgerNav_on").style.display = "block";
+}
+
+function burgermenu_on() {
+  document.getElementById("nav-links").style.display = "none";
+  document.getElementById("burgerNav_on").style.display = "none";
+  document.getElementById("burgerNav_off").style.display = "block";
 }
